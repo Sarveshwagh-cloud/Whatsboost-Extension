7fdbe92fb020170a951f9d5f4a0e2a1b4b98dd9b
Sarveshwagh-cloud <72179951+Sarveshwagh-cloud@users.noreply.github.com>
2024-08-16 13:00:30 +0530

    Add files via upload

--- background.js
+++ background.js
@@ -1,4 +1,30 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.create({ url: "https://whatsboost.in/user/dashboard" });
-  });
-  
+  const dashboardUrl = 'https://whatsboost.in/user/dashboard';
+  const mainUrl = 'https://whatsboost.in/';
+
+  // Check if the tab's URL is valid for injection
+  if (tab.url.startsWith('http://') || tab.url.startsWith('https://')) {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      func: checkDashboard,
+      args: [dashboardUrl, mainUrl]
+    });
+  } else {
+    // If the current tab is not valid, open a new tab directly
+    chrome.tabs.create({ url: mainUrl });
+  }
+});
+
+function checkDashboard(dashboardUrl, mainUrl) {
+  fetch(dashboardUrl, { method: 'GET', credentials: 'include' })
+    .then(response => {
+      if (response.ok) {
+        window.location.href = dashboardUrl;
+      } else {
+        window.location.href = mainUrl;
+      }
+    })
+    .catch(() => {
+      window.location.href = mainUrl;
+    });
+}
